fd9effc9c43f440b7d99e5b4ceeb6aa60d3351fb
Krasnyuk Sergey <17615311+Nixend-creator@users.noreply.github.com>
2026-01-20 15:54:57 +0200

    Fix error

--- src/main/java/com/n1xend/authlite/module/PlaceholderApiSupportModule.java
+++ src/main/java/com/n1xend/authlite/module/PlaceholderApiSupportModule.java
@@ -3,7 +3,6 @@ package com.n1xend.authlite.module;
 import com.n1xend.authlite.AuthLite;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
-import org.jetbrains.annotations.NotNull;
 
 public class PlaceholderApiSupportModule implements Module {
     private final AuthLite plugin;
@@ -24,20 +23,20 @@ public class PlaceholderApiSupportModule implements Module {
         }
     }
 
-    private class AuthLitePlaceholder extends PlaceholderExpansion {
+    public class AuthLitePlaceholder extends PlaceholderExpansion {
 
         @Override
-        public @NotNull String getIdentifier() {
+        public String getIdentifier() {
             return "authlite";
         }
 
         @Override
-        public @NotNull String getAuthor() {
+        public String getAuthor() {
             return "N1xend";
         }
 
         @Override
-        public @NotNull String getVersion() {
+        public String getVersion() {
             return plugin.getDescription().getVersion();
         }
 
@@ -47,17 +46,12 @@ public class PlaceholderApiSupportModule implements Module {
         }
 
         @Override
-        public String onPlaceholderRequest(OfflinePlayer player, @NotNull String params) {
-            if (player == null) {
-                return "";
-            }
-
+        public String onPlaceholderRequest(OfflinePlayer player, String params) {
+            if (player == null) return "";
             if (params.equalsIgnoreCase("logged_in")) {
                 return String.valueOf(plugin.isLoggedIn(player.getUniqueId()));
             }
-
-            // Важно: возвращай null для неизвестных параметров
-            return null;
+            return null; // важно: null для неизвестных параметров
         }
     }
 }
