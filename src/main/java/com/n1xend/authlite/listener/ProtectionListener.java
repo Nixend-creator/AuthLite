54da26c506694ce67cc444bfd4f6939a643a88a3
Krasnyuk Sergey <17615311+Nixend-creator@users.noreply.github.com>
2026-01-20 15:37:38 +0200

    Update ProtectionListener.java

--- src/main/java/com/n1xend/authlite/listener/ProtectionListener.java
+++ src/main/java/com/n1xend/authlite/listener/ProtectionListener.java
@@ -2,6 +2,7 @@ package com.n1xend.authlite.listener;
 
 import com.n1xend.authlite.AuthLite;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
