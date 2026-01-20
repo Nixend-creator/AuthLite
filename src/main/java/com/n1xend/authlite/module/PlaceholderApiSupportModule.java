9cfca213aa723096aedd95144c15387e2bff61ae
Krasnyuk Sergey <17615311+Nixend-creator@users.noreply.github.com>
2026-01-20 15:59:40 +0200

    Update PlaceholderApiSupportModule.java

--- src/main/java/com/n1xend/authlite/module/PlaceholderApiSupportModule.java
+++ src/main/java/com/n1xend/authlite/module/PlaceholderApiSupportModule.java
@@ -3,6 +3,7 @@ package com.n1xend.authlite.module;
 import com.n1xend.authlite.AuthLite;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
 
 public class PlaceholderApiSupportModule implements Module {
     private final AuthLite plugin;
@@ -26,17 +27,17 @@ public class PlaceholderApiSupportModule implements Module {
     public class AuthLitePlaceholder extends PlaceholderExpansion {
 
         @Override
-        public String getIdentifier() {
+        public @NotNull String getIdentifier() {
             return "authlite";
         }
 
         @Override
-        public String getAuthor() {
+        public @NotNull String getAuthor() {
             return "N1xend";
         }
 
         @Override
-        public String getVersion() {
+        public @NotNull String getVersion() {
             return plugin.getDescription().getVersion();
         }
 
@@ -46,12 +47,14 @@ public class PlaceholderApiSupportModule implements Module {
         }
 
         @Override
-        public String onPlaceholderRequest(OfflinePlayer player, String params) {
-            if (player == null) return "";
+        public String onPlaceholderRequest(OfflinePlayer player, @NotNull String params) {
+            if (player == null) {
+                return "";
+            }
             if (params.equalsIgnoreCase("logged_in")) {
                 return String.valueOf(plugin.isLoggedIn(player.getUniqueId()));
             }
-            return null; // важно: null для неизвестных параметров
+            return null; // ← важно: null для неизвестных параметров
         }
     }
 }
