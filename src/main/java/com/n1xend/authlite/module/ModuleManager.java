6b6edab6b9746cb0d67fdfa3d60cb4d95af86d1b
Krasnyuk Sergey <17615311+Nixend-creator@users.noreply.github.com>
2026-01-20 15:13:44 +0200

    Add additional modules to ModuleManager

--- src/main/java/com/n1xend/authlite/module/ModuleManager.java
+++ src/main/java/com/n1xend/authlite/module/ModuleManager.java
@@ -13,6 +13,21 @@ public class ModuleManager {
         this.plugin = plugin;
         modules.add(new CoreAuthModule(plugin));
         modules.add(new IpAccountLimitModule(plugin));
+        // В ModuleManager.java
+modules.add(new CoreAuthModule(plugin));
+modules.add(new AutoLoginModule(plugin));
+modules.add(new PasswordChangeModule(plugin));
+modules.add(new BruteForceProtectionModule(plugin));
+modules.add(new IpAccountLimitModule(plugin));
+modules.add(new HumanityCheckModule(plugin));
+modules.add(new SsrfProtectionModule(plugin));
+modules.add(new EmergencySessionRevokeModule(plugin));
+modules.add(new AuditLoggingModule(plugin));
+modules.add(new GeoIpBlockingModule(plugin));
+modules.add(new HttpApiModule(plugin));
+modules.add(new PlaceholderApiSupportModule(plugin));
+modules.add(new SandboxModeModule(plugin));
+modules.add(new AutoRecoveryModule(plugin));
     }
 
     public void loadAll() {
